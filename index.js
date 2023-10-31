132be721ab818a25b46651226f473111d9ae8ef0
ernesto.dainesi <ernesto.dainesi@avature.net>
2023-10-31 19:32:45 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const FORM_CLASS = "mascotas-form";
+const DELETE_BTN_CLASS = "eliminar-btn";
+const ADD_BTN_ID = "add-btn";
+const DELETE_PET_BTN_CLASS = "delete-pet";
+const HIDE_ADD_BTN_CSS = "hideAddBtn";
+let id = 0;
+
+const addButton = document.getElementById(ADD_BTN_ID);
+
+addButton.addEventListener('click', _ => {
+    const url = document.getElementById("url-input").value;
+    if (id >= 6) {
+        return;
+    }
+    if (addButton.classList.contains(HIDE_ADD_BTN_CSS)) {
+        addButton.classList.remove(HIDE_ADD_BTN_CSS);
+    }
+    id++;
+    const newImage = `
+        <div class="pet" id="pet-${id}">
+            <img src="${url}" alt="Foto de una mascota" width="300" height="300" class="pet-foto"/>
+            <button class="delete-pet" id="${id}">Eliminar</button>
+        </div>
+    `;
+    $("#mascotas-container").append(newImage);
+    alert("Nueva mascota agregada!");
+    if (id >= 6) {
+        addButton.classList.add(HIDE_ADD_BTN_CSS);
+        return;
+    }
+});
+
+const petContainer = document.getElementById("mascotas-container");
+
+petContainer.addEventListener('click', e => {
+    if (e.target.className !== DELETE_PET_BTN_CLASS) {
+        return;
+    }
+    if (id <= 6 && addButton.classList.contains(HIDE_ADD_BTN_CSS)) {
+        addButton.classList.remove(HIDE_ADD_BTN_CSS);
+    }
+    const petId = `#pet-${e.target.id}`;
+    $(petId).remove();
+    id--;
+});
+
+const removeBtn = document.getElementById("remove-btn");
+
+removeBtn.addEventListener('click', e => {
+    const petList = document.getElementsByClassName("pet");
+    const url = document.getElementById("url-input").value;
+    console.log(url);
+    for (pet of petList) {
+        console.log(pet)
+        if (pet.src === url) {
+            $(pet.id).remove();
+        }
+    }
+    if (id <= 6 && addButton.classList.contains(HIDE_ADD_BTN_CSS)) {
+        addButton.classList.remove(HIDE_ADD_BTN_CSS);
+    }
+});
